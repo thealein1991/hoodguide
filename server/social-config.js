21ecae924e067a1fae367d9eadc1fda1d6f45d54
thea_kitzinger <thealein1991@googlemail.com>
2015-11-17 22:47:57 +0100

    login Facebook and Registration form

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1902308306661626',
+    secret: '3c0661fdd69b704b8714862e1876e6bd'
+});
